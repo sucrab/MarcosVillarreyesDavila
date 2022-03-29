8b6df1c3cfcfb2649eaf4f73d99e978576225fbd
sucrab <victor_diaz8@usmp.pe>
2022-03-29 17:08:34 -0500

    Version 3 estilo wb

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ const selectElement = (element) => document.querySelector(element);
 
 selectElement('.mobile-menu').addEventListener('click', () => {
   selectElement('header').classList.toggle('active');
-})
+});
