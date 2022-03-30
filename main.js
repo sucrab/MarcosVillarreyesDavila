1ff984f37af66a190259fd691b0de32bfdec1e2a
sucrab <victor_diaz8@usmp.pe>
2022-03-30 18:21:40 -0500

    version 4

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-//JavaScript
-
-const selectElement = (element) => document.querySelector(element);
-
-selectElement('.mobile-menu').addEventListener('click', () => {
-  selectElement('header').classList.toggle('active');
+window.addEventListener('load',function () {
+  new Glider(document.querySelector('.carusel_lista'),{
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: '.carusel_indicadores',
+    arrows: {
+      prev: '.carusel_anterior',
+      next: '.carusel_siguiente'
+    },
+    responsive: [
+    {
+      // screens greater than >= 800px
+      breakpoint: 450,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    }
+  ]
+  });
 });
